68d46390a58f11a7892e7a3f7b92d0563473434d
gadzhi <gadzhidadaev95@gmail.com>
2019-04-12 14:44:33 +0300

    lesson 2.5

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,30 @@
+'use strict';
 
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const webpack = require('webpack');
 
 module.exports = {
+  
+  mode: 'production',
+
   entry: './home.js',
   output: {
-    filename: 'build.js'
-  }
+    filename: 'build.js',
+    library: 'home'
+  },
+
+  watch: NODE_ENV == 'development',
+
+  watchOptions: {
+    aggregateTimeout: 100
+  },
+
+  devtool: NODE_ENV == 'development' ? 'cheap-inline-module-source-map' : null,
+
+  plugins: [
+    new webpack.DefinePlugin({
+      NODE_ENV: JSON.stringify(NODE_ENV),
+      LANG: JSON.stringify('ru')
+    })
+  ]
 };
