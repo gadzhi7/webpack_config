f38941d11ea468ffe58835d5262191146d9f7488
gadzhi <gadzhidadaev95@gmail.com>
2019-04-30 15:54:16 +0300

    simple setting

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,47 @@
-'use strict';
-
-const webpack = require('webpack');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-
-
-// при продакшене только добавлять hash
-
-
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
-  context: __dirname + '/frontend',
-
-  entry: {
-    app: './app',
-  },
-
+  entry: { main: './src/index.js' },
   output: {
-    path: __dirname + '/public/assets',
-    publicPath: '/assets/',
-    filename: '[name].js',
-    chunkFilename: '[id].js',
-    library: '[name]'
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js'
   },
-
-  mode: 'development',
-
-  watch: true,
-  watchOptions: {
-    aggregateTimeout: 100
-  },
-
-  plugins: [
-    // плагин когда выдает ошибку не компилирует файлы
-    new webpack.NoEmitOnErrorsPlugin(),
-  ],
-
-  //оптимизирует код, создает отдельный js файл с общим(одниковым у всех файлов) кодом
-  optimization: {
-    // minimize output files
-    minimize: false,
-    minimizer: [
-      new UglifyJSPlugin({
-        cache: false,
-        sourceMap: true,
-        extractComments: true
-      })
-    ],
-    runtimeChunk: false,
-    splitChunks: {
-      cacheGroups: {
-        commons: {
-          chunks: 'all',
-          name: 'commons',
-          test: 'commons',
-          minChunks: 2
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader?optional[]=runtime',
+          options: {
+            presets: ['@babel/preset-env']
+          }
         }
+      },
+      {
+        test: /\.scss$/,
+        use:  [  'style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
       }
-    }
+    ]
   },
-
-};
+  plugins: [
+    new CleanWebpackPlugin(),
+    // new ExtractTextPlugin(
+    //   {filename: 'style.[chunkhash].css', disable: false, allChunks: true}
+    // ),
+    new MiniCssExtractPlugin({
+      filename: 'style.[contenthash].css',
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    new WebpackMd5Hash()
+  ]
+}
